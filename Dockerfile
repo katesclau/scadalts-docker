fcb21ec208bd512718024133862d5b90f1a4f5ba
Diego Ferreira <katesclau@gmail.com>
2018-04-30 18:26:12 -0300

    Adds context and mysql-connectors to the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,22 @@ FROM ubuntu:16.04
 
 MAINTAINER Diego Ferreira version: 0.1
 
-RUN apt-get update && apt-get install -y openjdk-8-jre  mysql-client mariadb-server tomcat7 && apt-get clean && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y dos2unix unzip openjdk-8-jre  mariadb-client-10.0 mariadb-server-10.0 tomcat7 && apt-get clean && rm -rf /var/lib/apt/lists/*
 
 ENV TOMCAT_RUN_USER tomcat7
 ENV TOMCAT_RUN_GROUP tomcat7
 ENV TOMCAT_LOG_DIR /var/log/tomcat7
 ENV CATALINA_HOME /var/lib/tomcat7
 
-ADD ./scadalts-setup.sh /tmp/scadalts-setup.sh
 ADD ./ScadaBR.war /tmp/ScadaBR.war
 ADD ./ScadaLTS.war /tmp/ScadaLTS.war
+ADD ./context.xml /var/lib/tomcat7/conf
+ADD ./mysql-connector-java-* /usr/share/tomcat7/lib/
 ADD ./start.sh /tmp/start.sh
 
-EXPOSE 8090
+# Ensures that our script is runnable
+RUN dos2unix /tmp/start.sh
+
+EXPOSE 8080
 
 CMD /tmp/start.sh
--- start.sh
+++ start.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
+
+# This script depends on DOCKER_HOST_IP
+if [ -z "$DOCKER_HOST_IP" ]; then
+    echo "Need to set DOCKER_HOST_IP"
+    exit 1
+fi
+
 chown -R mysql:mysql /var/lib/mysql /var/run/mysql
 /etc/init.d/mysql start
 
-ENV_PROPS=/var/lib/tomcat7/webapps/ScadaLTS/WEB-INF/classes/env.properties
+WEBAPPS=/var/lib/tomcat7/webapps
+ENV_PROPS="$WEBAPPS/ScadaBR/WEB-INF/classes/env.properties"
+
+# Unpack WAR files into webapps
+# ScadaBR
+mkdir /var/lib/tomcat7/webapps/ScadaBR
+cd /var/lib/tomcat7/webapps/ScadaBR/
+unzip /tmp/ScadaBR.war
+
+# ScadaLTS
+mkdir /var/lib/tomcat7/webapps/ScadaLTS
+cd /var/lib/tomcat7/webapps/ScadaLTS/
+unzip /tmp/ScadaLTS.war
+
+# Grant permission
+chown -R tomcat7.tomcat7 /var/lib/tomcat7/webapps
 
 # create database scadalts if not exist
 sleep 10
-mysql -uroot -proot -e "create database if not exists scadalts"
+mysql -uroot -proot -e "create user 'scadalts'@'localhost' identified by 'scadalts'; \
+    create database if not exists scadalts; \
+    grant all privileges on scadalts.* to 'scadalts'@'localhost'; \
+    flush privileges;"
 
 # Update the IP in the env.properties
-sed -i 's/localhost:8090/'`echo $DOCKER_HOST_IP`:8090'/g' $ENV_PROPS
+sed -i 's/localhost:8080/'`echo $DOCKER_HOST_IP`:8080'/g' $ENV_PROPS
 
 # Start the tomcat server
 /etc/init.d/tomcat7 start
